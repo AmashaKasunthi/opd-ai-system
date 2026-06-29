1969810f157aa261164bb7b20e0a69d8203ce273
AmashaKasunthi <akasunthi@gmail.com>
2026-06-29 20:42:02 +0530

    update code

--- src/main/java/com/opd/opd_ai_system/controller/AuthController.java
+++ src/main/java/com/opd/opd_ai_system/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.opd.opd_ai_system.controller;
 
-import com.opd.opd_ai_system.dto.LoginRequest;
-import com.opd.opd_ai_system.dto.LoginResponse;
+import com.opd.opd_ai_system.dto.*;
+import com.opd.opd_ai_system.service.PasswordResetService;
 import com.opd.opd_ai_system.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +12,39 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
-    @Autowired
-    UserService userService;
+    private final UserService userService;
+    private final PasswordResetService passwordResetService;
 
-    @PostMapping("/login")
-
-    public LoginResponse login(
-            @RequestBody LoginRequest request){
-
-        return userService.login(request);
-
+    public AuthController(
+            UserService userService,
+            PasswordResetService passwordResetService) {
+        this.userService = userService;
+        this.passwordResetService = passwordResetService;
     }
 
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        return userService.login(request);
+    }
+
+    @PostMapping("/forgot-password")
+    public String forgotPassword(@RequestBody OtpRequest request) {
+        return passwordResetService.sendOtp(request.getEmail());
+    }
+
+    @PostMapping("/verify-otp")
+    public String verifyOtp(@RequestBody VerifyOtpRequest request) {
+        return passwordResetService.verifyOtp(
+                request.getEmail(),
+                request.getOtp()
+        );
+    }
+
+    @PostMapping("/reset-password")
+    public String resetPassword(@RequestBody ResetPasswordRequest request) {
+        return passwordResetService.resetPassword(
+                request.getEmail(),
+                request.getNewPassword()
+        );
+    }
 }
