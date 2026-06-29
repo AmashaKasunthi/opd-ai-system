3632f4bf8228d7ca5c810610804bfd6525b015f1
AmashaKasunthi <akasunthi@gmail.com>
2026-06-29 20:42:55 +0530

    update loginside code

--- src/main/java/com/opd/opd_ai_system/dto/LoginRequest.java
+++ src/main/java/com/opd/opd_ai_system/dto/LoginRequest.java
@@ -4,11 +4,21 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
-
 public class LoginRequest {
 
     private String email;
     private String password;
+    private String role;
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
--- src/main/java/com/opd/opd_ai_system/dto/LoginResponse.java
+++ src/main/java/com/opd/opd_ai_system/dto/LoginResponse.java
@@ -13,4 +13,7 @@ public class LoginResponse {
     private String message;
     private String role;
 
+    public LoginResponse() {
+
+    }
 }
