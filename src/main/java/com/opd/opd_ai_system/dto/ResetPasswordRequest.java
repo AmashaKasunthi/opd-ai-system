f9bc6827982008d5c263d3484d675a1dd405b861
AmashaKasunthi <akasunthi@gmail.com>
2026-08-11 15:31:20 +0530

    Updated password reset side

--- src/main/java/com/opd/opd_ai_system/dto/ResetPasswordRequest.java
+++ src/main/java/com/opd/opd_ai_system/dto/ResetPasswordRequest.java
@@ -1,9 +1,23 @@
 package com.opd.opd_ai_system.dto;
 
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class ResetPasswordRequest {
+
     private String email;
+    private String otp;
     private String newPassword;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
 }
