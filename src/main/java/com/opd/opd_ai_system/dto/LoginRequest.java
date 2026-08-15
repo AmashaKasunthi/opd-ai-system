2ee608816aa9d17a29289a80bd2bbb1eaacf95f1
AmashaKasunthi <akasunthi@gmail.com>
2026-08-15 11:26:52 +0530

    Update login request DTO

--- src/main/java/com/opd/opd_ai_system/dto/LoginRequest.java
+++ src/main/java/com/opd/opd_ai_system/dto/LoginRequest.java
@@ -3,6 +3,7 @@ package com.opd.opd_ai_system.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+@Setter
 @Getter
 public class LoginRequest {
 
@@ -10,15 +11,4 @@ public class LoginRequest {
     private String password;
     private String role;
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
